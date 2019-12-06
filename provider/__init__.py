ed217fea76c4d0f69b27352fae6746a90bc7045f
Alexander Schulze <cibernop@gmail.com>
2019-12-06 22:16:09 +0100

    moved provider aggregation into package

--- provider/__init__.py
+++ provider/__init__.py
@@ -1,1 +1,22 @@
-from provider.immobilienscout24 import get_apartments
+import pathlib
+
+
+pwd = pathlib.Path(__file__).parent
+
+
+def get_apartments(config, cfg):
+    ignore = ['listing', '__init__']
+
+    apartments = {}
+    for child in pwd.iterdir():
+        if child.is_file() and child.stem not in ignore and child.suffix == '.py':
+            name = child.stem
+            imported = getattr(__import__('provider', fromlist=[name]), name)
+            res = imported.get_apartments(config, cfg[name])
+
+            for quarter, aps in res.items():
+                if quarter not in apartments:
+                    apartments[quarter] = []
+                apartments[quarter] += aps
+
+    return apartments
