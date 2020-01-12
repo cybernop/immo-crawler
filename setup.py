2030c07a6215bb1c2c7fcebdfe1b6f30517061b8
Alexander Schulze <cibernop@gmail.com>
2020-01-12 10:00:03 +0100

    clean-up of setup.py and re-introduce requirements.txt

--- setup.py
+++ setup.py
@@ -1,20 +1,17 @@
 #!/usr/bin/env python
-from distutils.core import setup
+import setuptools
 
-setup(name='immocrawler',
-      version='0.1',
-      description='Python notifications for Telegram',
-      author='Alexander Schulze',
-      author_email='cibernop@gmail.com',
-      packages=[
-          'immocrawler',
-          'immocrawler.inout',
-          'immocrawler.provider',
-      ],
-      install_requires=[
-          'pandas==0.25.1',
-          'PyYAML==5.1.2',
-          'python-telegram-bot==12.2.0',
-          'requests==2.22.0',
-      ]
-      )
+setuptools.setup(
+    name='immocrawler',
+    version='0.1',
+    description='Python notifications for Telegram',
+    author='Alexander Schulze',
+    author_email='cibernop@gmail.com',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'pandas',
+        'PyYAML',
+        'python-telegram-bot',
+        'requests',
+    ]
+)
