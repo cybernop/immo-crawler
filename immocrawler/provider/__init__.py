a3f86cc2c6a047c1ef02279e768603af3a89ab40
Alexander Schulze <cibernop@gmail.com>
2020-01-14 19:46:48 +0100

    fixed package name for dynamically importing provider packages

--- immocrawler/provider/__init__.py
+++ immocrawler/provider/__init__.py
@@ -12,7 +12,7 @@ def get_apartments(config):
     for child in pwd.iterdir():
         if child.is_file() and child.stem not in ignore and child.suffix == '.py' and not child.stem.startswith("test_"):
             name = child.stem
-            imported = getattr(__import__('provider', fromlist=[name]), name)
+            imported = getattr(__import__('immocrawler.provider', fromlist=[name]), name)
             res = imported.get_apartments(config, config.providers[name])
             apartments.update(res)
     return apartments
