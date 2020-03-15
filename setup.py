39cb32aa9727b2011113abe49b718ab9d9f8185e
Alexander Schulze <cibernop@gmail.com>
2020-03-15 20:56:41 +0100

    bump version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='immocrawler',
-    version='0.1',
+    version='1.0.1',
     description='Python notifications for Telegram',
     author='Alexander Schulze',
     packages=setuptools.find_packages(),
